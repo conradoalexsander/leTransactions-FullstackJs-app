f9cbe072731a9f8f423a79a537e429189fe2f054
Conrado Alexsander <conradoalexsander@hotmail.com>
2020-07-18 23:04:09 -0300

    fixed index title and baseURL port

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_PORT || 'https://letransactions-desafio-final.herokuapp.com/api/transaction',
+  baseURL: process.env.REACT_APP_PORT || '3001',
 });
 
 export default api;
